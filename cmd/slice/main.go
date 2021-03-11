12720cc3b3614071482b4ed60ced490d8aae7dcd
scottcagno <scottiecagno@gmail.com>
2021-03-11 15:54:13 -0500

    first

---
+++ cmd/slice/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"os"
+	"text/template"
+)
+// https://play.golang.org/p/Jn4XQgcOEsk
+
+type data struct {
+	Type string
+	Name string
+}
+
+//go:embed slice.tmpl
+var sliceTmpl string
+
+func main() {
+
+	var d data
+	flag.StringVar(&d.Type, "type", "", "The subtype used for the slice being generated")
+	flag.StringVar(&d.Name, "name", "","The name used for the slice being generated.")
+	flag.Parse()
+
+	t := template.Must(template.New("slice").Parse(sliceTmpl))
+	t.Execute(os.Stdout, d)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/scottcagno/generators"
+
+go 1.16
